23c3e72a00fc990766718291c7c2769315cc2e53
PatrickVHL <80406686+PatrickVHL@users.noreply.github.com>
2022-03-21 12:52:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
