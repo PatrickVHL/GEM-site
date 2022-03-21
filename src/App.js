6924026e81f95cf1de2c6c6cf8fb97e57a2ae1ea
PatrickVHL <80406686+PatrickVHL@users.noreply.github.com>
2022-03-21 12:52:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import Pricing from "./components/Pricing.jsx";
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <div className="container py-3">
+        <Header />
+        <Pricing />
+        <Footer />
+      </div>
     </div>
   );
 }
